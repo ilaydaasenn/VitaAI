21d17cea91df2bf9da8ca2a74b1053551c63b0c9
ilaydaasenn <ilyydasen@gmail.com>
2026-03-06 21:51:31 +0300

    Docker veritabanı kuruldu ve bağlantı sağlandı

---
+++ backend/database.py
@@ -0,0 +1,15 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+# Docker üzerinde çalışan veritabanına bağlanmak için gerekli adres bilgileri.
+SQLALCHEMY_DATABASE_URL = "postgresql://vitaai_admin:VitaAI_20262808@localhost:5432/vitaai_db"
+
+# Veritabanı ile Python arasındaki iletişimi sağlayan ana bağlantı.
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+# Veritabanı işlemleri için kullanılacak oturumları(session) yöneten yapı.
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# Veritabanındaki tabloları Python sınıfları olarak tanımlarken kullanılacak ana sınıf.
+Base = declarative_base()
